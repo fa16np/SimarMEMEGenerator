7d5b8551622cd906543d8b5aec4ffb4fd8ee264f
Kindeep <kindeepsingh@gmail.com>
2019-03-15 20:27:51 -0400

    some kinda face shit

--- app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
+++ app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
@@ -1,12 +1,15 @@
 package me.kindeep.simarmemegenerator;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.FaceDetector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
 
     @Override
@@ -14,12 +17,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.obama);
 
-        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
-                .setTrackingEnabled(false)
-                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
-                .build();
+        FaceDetector detector = new FaceDetector(image.getWidth(), image.getHeight(), 5);
+
+        FaceDetector.Face[] faces = new FaceDetector.Face[5];
+
+        detector.findFaces(image, faces);
 
 
+        Log.v("YOOOO", Integer.toString(faces.length) + faceToString(faces));
+
+
+    }
+
+    protected String faceToString(FaceDetector.Face[] faces) {
+        String result = "";
+        for (FaceDetector.Face face : faces) {
+            if (face != null)
+                result += Double.toString(face.confidence());
+            else result += "fuck";
+        }
+        return result;
     }
 }
