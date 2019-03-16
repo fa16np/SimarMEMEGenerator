967da9fc254d91def05c5016f8668275dc05dcec
Fahad Ansar <fa16np@brocku.ca>
2019-03-16 00:00:17 -0400

    Face dal dia hai

--- app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
+++ app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
@@ -1,13 +1,25 @@
 package me.kindeep.simarmemegenerator;
 
+import android.media.FaceDetector;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+
     }
 }
