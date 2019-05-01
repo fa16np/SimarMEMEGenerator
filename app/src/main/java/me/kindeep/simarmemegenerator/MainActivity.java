bad0829b1be17182d435bf545f8254ba2be34670
Kindeep <kk17xg@brocku.ca>
2019-05-01 16:50:34 -0400

    fahad

--- app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
+++ app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
@@ -3,7 +3,13 @@ package me.kindeep.simarmemegenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        FaceDetector detector = new FaceDetector.Builder()
-//                .build(getApplicationContext());
+        FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
+                .setProminentFaceOnly(true)
+                .build();
 
-
+//        detector.setProcessor(
+//                new LargestFaceFocusingProcessor(
+//                        detector,
+//                        new FaceTracker())
+//        );
+//
+//        CameraSource cameraSource = new CameraSource.Builder(context, detector)
+//                .setFacing(CameraSource.CAMERA_FACING_FRONT)
+//                .setRequestedPreviewSize(320, 240)
+//                .build()
+//                .start();
 //        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.obama);
 //
 ////        FaceDetector detector = new FaceDetector(image.getWidth(), image.getHeight(), 5);
