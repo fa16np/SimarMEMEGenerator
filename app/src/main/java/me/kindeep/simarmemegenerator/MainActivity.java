fb44eae2be4d32e438fef09b96b4ee2e67e797df
Kindeep <kk17xg@brocku.ca>
2019-05-01 16:15:54 -0400

    compiles

--- app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
+++ app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
@@ -1,13 +1,9 @@
 package me.kindeep.simarmemegenerator;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.FaceDetector;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import java.io.Console;
+import com.google.android.gms.vision.face.FaceDetector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,27 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.obama);
-
-        FaceDetector detector = new FaceDetector(image.getWidth(), image.getHeight(), 5);
-
-        FaceDetector.Face[] faces = new FaceDetector.Face[5];
-
-        detector.findFaces(image, faces);
+//        FaceDetector detector = new FaceDetector.Builder()
+//                .build(getApplicationContext());
 
 
-        Log.v("YOOOO", Integer.toString(faces.length) + faceToString(faces));
+//        Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.obama);
+//
+////        FaceDetector detector = new FaceDetector(image.getWidth(), image.getHeight(), 5);
+//        FaceDetector detector = new FaceDetector.Builer(context);
+
+//        FaceDetector.Face[] faces = new FaceDetector.Face[5];
+//
+//        detector.findFaces(image, faces);
 
 
-    }
+//        Log.v("YOOOO", Integer.toString(faces.length) + faceToString(faces));
 
-    protected String faceToString(FaceDetector.Face[] faces) {
-        String result = "";
-        for (FaceDetector.Face face : faces) {
-            if (face != null)
-                result += Double.toString(face.confidence());
-            else result += "fuck";
-        }
-        return result;
+
     }
 }
