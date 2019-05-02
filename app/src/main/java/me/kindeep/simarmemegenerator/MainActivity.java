f9edc77ee948c1be4bca02fdfaa1da483198ae80
Kindeep <kk17xg@brocku.ca>
2019-05-02 17:16:37 -0400

    upload dialog opens

--- app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
+++ app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
@@ -1,50 +1,59 @@
 package me.kindeep.simarmemegenerator;
 
+import android.content.Intent;
 import android.graphics.Matrix;
-import android.graphics.Rect;
-import android.support.v4.graphics.BitmapCompat;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
 
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.MultiProcessor;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
-import com.google.android.gms.vision.face.LargestFaceFocusingProcessor;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
-import android.os.Bundle;
-import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    final int OPEN_FILE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button open = findViewById(R.id.load_button);
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("OPEN FILE GOD DAMNIT");
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+
+                startActivityForResult(chooserIntent, OPEN_FILE);
+            }
+        });
+
         FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
                 .setProminentFaceOnly(true)
                 .build();
 
-        ImageView myImageView = (ImageView) findViewById(R.id.imgview);
+        imageView = (ImageView) findViewById(R.id.imgview);
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inMutable = true;
         Bitmap base_image_temp = BitmapFactory.decodeResource(
@@ -91,11 +100,15 @@ public class MainActivity extends AppCompatActivity {
             float y_top = x1 - thisFace.getHeight() / 2;
             Bitmap temp_bitmap_simar = Bitmap.createBitmap((int) thisFace.getWidth(), (int) thisFace.getHeight(), Bitmap.Config.RGB_565);
 
-            resultCanvas.drawBitmap(getResizedBitmap(simar_temp, (int)thisFace.getWidth(), (int)thisFace.getHeight()), x1, y1, null);
+            resultCanvas.drawBitmap(getResizedBitmap(simar_temp, (int) thisFace.getWidth(), (int) thisFace.getHeight()), x1, y1, null);
         }
 
-        myImageView.setImageDrawable(new BitmapDrawable(getResources(), resultBitmap));
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), resultBitmap));
 
+    }
+
+    void setImageViewBitmap(Bitmap bitmap) {
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), bitmap));
     }
 
     public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
@@ -113,4 +126,33 @@ public class MainActivity extends AppCompatActivity {
 //        bm.recycle();
         return resizedBitmap;
     }
+
+    void showToast(Object a) {
+        Toast.makeText(this, a.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == OPEN_FILE) {
+            final Bundle extras = data.getExtras();
+            if (extras != null) {
+                //Get image
+                Bitmap image = extras.getParcelable("data");
+                setImageViewBitmap(image);
+            }
+        }
+    }
+
+    public void FromCamera() {
+
+//        Log.i("camera", "startCameraActivity()");
+//        File file = new File(path);
+//        Uri outputFileUri = Uri.fromFile(file);
+//        Intent intent = new Intent(
+//                android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+//        startActivityForResult(intent, 1);
+
+    }
+
 }
