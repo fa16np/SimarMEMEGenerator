142e029a5509001359ca019f3e96d2cdc4fc7f98
Kindeep <kk17xg@brocku.ca>
2019-09-14 16:37:19 -0400

    idk some

--- app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
+++ app/src/main/java/me/kindeep/simarmemegenerator/MainActivity.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 
 import com.google.android.gms.vision.Frame;
@@ -28,11 +29,10 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
 
-    Bitmap simar_temp;
-
-    Bitmap base_image_temp;  //Main Image
-
     final int OPEN_FILE = 1;
+    private final double PATKA_RATIO = 0.2;
+
+    BitmapFactory.Options options;
 
     //First method get opened when you run app--------------------
     @Override
@@ -42,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Making a button----------------------------------------
         Button open = findViewById(R.id.load_button);
-
 
 
         //click listener for button--------------------------------
@@ -57,87 +56,110 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        imageView = (ImageView) findViewById(R.id.imgview);
 
-        //Using face detector for detecting all the faces in the photo--------
+        options = new BitmapFactory.Options();
+        options.inMutable = true;
+
+        imageView.setImageBitmap(BitmapFactory.decodeResource(getApplicationContext().getResources(),
+                R.raw.lead_720_405, options));
+
+//        simarify();
+
+    }
+
+    public void simarify(View view) {
+        simarify();
+    }
+
+    public void simarify() {
+//        Bitmap img = BitmapFactory.decodeResource(getApplicationContext().getResources(),
+////                R.id.imgview, options);
+        Bitmap img = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+
         FaceDetector detector = new FaceDetector.Builder(getApplicationContext())
                 .setProminentFaceOnly(true)
                 .build();
 
+        //Image veiw for showing images
 
+        Bitmap resultBitmap = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.RGB_565);
 
-        //Image veiw for showing images---------------------------------------
-        imageView = (ImageView) findViewById(R.id.imgview);
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inMutable = true;
-
-        base_image_temp = BitmapFactory.decodeResource(
-                getApplicationContext().getResources(),
-                R.raw.lead_720_405,
-                options);
-
-        Bitmap resultBitmap = Bitmap.createBitmap(base_image_temp.getWidth(), base_image_temp.getHeight(), Bitmap.Config.RGB_565);
-
-
-
-        //Making canvas for image editing and then showing it later----------
+        //Making canvas for image editing and then showing it later
         Canvas resultCanvas = new Canvas(resultBitmap);
-        resultCanvas.drawBitmap(base_image_temp, 0, 0, null);
+        resultCanvas.drawBitmap(img, 0, 0, null);
 
 
-        simar_temp = BitmapFactory.decodeResource(
+        Bitmap simar_temp = BitmapFactory.decodeResource(
                 getApplicationContext().getResources(),
                 R.drawable.simar,
                 options);
 
+        Bitmap simarBitmap = Bitmap.createBitmap(simar_temp.getWidth(), simar_temp.getHeight(), Bitmap.Config.RGB_565);
 
         FaceDetector faceDetector = new
                 FaceDetector.Builder(this).setTrackingEnabled(false)
                 .build();
         if (!faceDetector.isOperational()) {
+//            new AlertDialog.Builder(getApplicationContext()).setMessage("Could not set up the face detector!").show();d\\
             Toast.makeText(this, "Could not set up face detector!", Toast.LENGTH_SHORT).show();
             return;
         }
 
 
-        Frame frame = new Frame.Builder().setBitmap(base_image_temp).build();
+        Frame frame = new Frame.Builder().setBitmap(img).build();
 
-        //All the faces-----------------------------------------------------
+        //All the faces
         SparseArray<Face> faces = faceDetector.detect(frame);
 
+        //Making paint tool
+        Paint myRectPaint = new Paint();
+//        myRectPaint.setColor(70);
 
-        //Running for loop on faces to edit and editing it one by one--------
+        //Running for loop on faces to edit and editing it one by one
         for (int i = 0; i < faces.size(); i++) {
             Face thisFace = faces.valueAt(i);
+
+            float fwidth = thisFace.getWidth();
+            float fheight = thisFace.getHeight();
+
             float x1 = thisFace.getPosition().x;
             float y1 = thisFace.getPosition().y;
-            float x2 = x1 + thisFace.getWidth();
-            float y2 = y1 + thisFace.getHeight();
+            float x2 = x1 + fwidth;
+            float y2 = y1 + fheight;
 
             float x_left = x1 - thisFace.getWidth() / 2;
             float y_top = x1 - thisFace.getHeight() / 2;
             Bitmap temp_bitmap_simar = Bitmap.createBitmap((int) thisFace.getWidth(), (int) thisFace.getHeight(), Bitmap.Config.RGB_565);
 
-            resultCanvas.drawBitmap(getResizedBitmap(simar_temp, (int) thisFace.getWidth(), (int) thisFace.getHeight()), x1, y1, null);
+            float orig_height = (thisFace.getHeight());
+            float final_height = (orig_height * (1 + (float) PATKA_RATIO));
+
+
+            resultCanvas.drawBitmap(getResizedBitmap(simar_temp, (int) thisFace.getWidth(), (int) final_height), x1, y1 - (final_height - orig_height), null);
+
+            Log.e("FACE", "FAce");
         }
 
         imageView.setImageDrawable(new BitmapDrawable(getResources(), resultBitmap));
-
     }
 
 
     /*
-    * Selects image from gallery and loads it for the processing
-    * */
-    public void  selectImageFromGallery(){
+     * Selects image from gallery and loads it for the processing
+     * */
+    public void selectImageFromGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(galleryIntent, OPEN_FILE);
+
+        simarify();
     }
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if( requestCode == OPEN_FILE){
+        if (requestCode == OPEN_FILE) {
             Toast.makeText(this, "YES", Toast.LENGTH_LONG).show();
             Uri imageData = data.getData();
             try {
@@ -155,10 +177,6 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
-
-
-
 
 
     //Setting image veiw
@@ -187,8 +205,6 @@ public class MainActivity extends AppCompatActivity {
     void showToast(Object a) {
         Toast.makeText(this, a.toString(), Toast.LENGTH_SHORT).show();
     }
-
-
 
 
     //--------------------------------------------Coming soon!
